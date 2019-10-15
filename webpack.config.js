0b499e49446a2a9a576bcef5a3a9047241958491
magalidefelippe <54870170+magalidefelippe@users.noreply.github.com>
2019-10-15 10:14:48 -0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/principal.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath:'/dist/'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
+  mode:'development'
+};
